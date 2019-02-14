37c6a27b8286c048396daeb58ce471406ce6141f
Даниил Волошин <kuravasic@gmail.com>
2019-02-14 09:06:49 +0300

    Initial Commit

--- Handwriting/main.cpp
+++ Handwriting/main.cpp
@@ -7,9 +7,13 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    
+    
+    
+    cout << "Wasuuuuup" << endl;
     return 0;
 }
